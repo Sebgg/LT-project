4180e2693e5ef0fcae6dee6fbbcdcb37bf181429
Sebastian Grunditz <sebgr273@student.liu.se>
2021-03-08 14:25:34 +0100

    added statistics display, and a persistant counter for # of evaluations

--- evaluator.py
+++ evaluator.py
@@ -4,25 +4,17 @@
 3. ask what language(s) the evaluator speaks, commaseparated
 4. randomize translated sentences from the applicable languages, evaluation counter
 5. save scores to the loaded score json file
-"""
 
-"""
 Combine two score sheets with:
-$ python3 evaluator.py (combine || -c) <original_data> <data_to_merge>
+$ python3 evaluator.py (--combine || -c) <original_data> <data_to_merge>
+
+Show statistics from a json file containing scores:
+$ python3 evaluator.py (--statistics || -s) <filename>
 
 Run the evaluator with:
 $ python3 evaluator.py
 """
 
-"""
----------------
-original text
----------------
-translated text
----------------
-score (1...5):
----------------
-"""
 import json
 import random
 import sys
@@ -36,8 +28,11 @@ class Evaluator():
     scores = {}
     score_file_name = "scores.json"
     translation_file_name = "translations.json"
+    counter_file_name = "cup.txt"
+    counter = 0
 
     def __init__(self):
+        atexit.register(self.exit_handler)
         print("\033c")
         print("Welcome to the translation evaluator!\nIt gives you a random ",\
             "to evaluate in one of the languages you know, then you get the\n",\
@@ -50,25 +45,67 @@ class Evaluator():
 
         with open(self.score_file_name, encoding='utf-8-sig') as f:
             self.scores = json.load(f)
+            
+        with open(self.counter_file_name, 'r', encoding='utf-8') as f:
+            temp = f.readline()
+            if temp != None and temp != "":
+                self.counter = int(temp)
 
         lang_temp = input("Write all languages you want to " + \
             "evaluate translation in, separated by a Space." + \
             " End by pressing Enter. \n> ")
 
         self.languages = lang_temp.lower().split()
-        atexit.register(self.exit_handler)
-        # Dict -> List -> Dict
-        # load json files here
-        # ask languages
         
     def exit_handler(self):
+        self.save_counter()
         self.dump_json()
 
     def dump_json(self):
         with open(self.score_file_name, 'w+', encoding='utf-8-sig') as f:
             json.dump(self.scores, f)
 
-    def combine_scores(self, original, new):
+    def save_counter(self):
+        with open(self.counter_file_name, 'w', encoding='utf-8') as f:
+            f.write(str(self.counter))
+    
+    def evaluate_translations(self):
+        while True:
+            self.evaluate_translation()
+            time.sleep(2)
+
+    def evaluate_translation(self):
+        language = random.choice(self.languages)
+        
+        translator = random.choice(list(self.translated.keys()))
+        iid = self.counter%129
+        original_lang = self.translated[translator][iid]["original language"]
+
+        if original_lang in self.languages:
+            sentence = '\n'.join(wrap(self.translated[translator][iid][language], 80))
+            original = '\n'.join(wrap(self.translated[translator][iid]["original"], 80))
+
+            if sentence != "N/A" and self.translated[translator][iid]:
+                print("\033c")
+                print(f'Evaluation #{self.counter}')
+                print("The Translation:\n"+"="*80)
+                print(sentence+"\n"+"="*80)
+                print("The Original sentence:\n"+"="*80)
+                print(original+"\n"+"="*80)
+                score = input("How good is the translation? (1 -> 5, 0 to skip the sentence)\n> ")
+                if int(score) >= 1:
+                    self.scores[translator].append({
+                        "score": score,
+                        "id": iid,
+                        "language": language
+                    })
+                    print(f"That translation was from {translator}!")
+                elif int(score) < 0:
+                    print("Invalid score!")
+        self.counter += 1
+        
+
+def combine_scores(original, new):
         with open(original, encoding='utf-8-sig') as f:
             original_data = json.load(f)
         
@@ -80,45 +117,41 @@ class Evaluator():
 
         with open(original, 'w+', encoding='utf-8-sig') as f:
             json.dump(original_data, f)
-    
-    def evaluate_translations(self):
-        while True:
-            self.evaluate_translation()
-            time.sleep(2)
 
-    def evaluate_translation(self):
-        language = random.choice(self.languages)
-        sentence_id = random.randint(0, 128)
-        translator = random.choice(list(self.translated.keys()))
+def show_statistics(filename):
+    scores = {}
+    with open(filename, encoding='utf-8-sig') as f:
+        scores = json.load(f)
 
-        original_lang = self.translated[translator][sentence_id]["original language"]
-        if original_lang in self.languages:
+    for translator, scores in scores.items():
+        all_scores = []
+        sentences = set()
+        for score in scores:
+            all_scores.append(int(score["score"]))
+            sentences.add(int(score['id']))
+
+        all_scores.sort()
+        mean = 0
+        if len(all_scores) > 0:
+            mean = float(sum(all_scores)/len(all_scores))
         
-            sentence = '\n'.join(wrap(self.translated[translator][sentence_id][language], 80))
-            original = '\n'.join(wrap(self.translated[translator][sentence_id]["original"], 80))
-
-            if sentence != "N/A" and self.translated:
-                print("\033c")
-                print("The Translation:\n"+"="*80)
-                print(sentence+"\n"+"="*80)
-                print("The Original sentence:\n"+"="*80)
-                print(original+"\n"+"="*80)
-                score = input("How good is the translation? (1 -> 5, 0 to skip the sentence)\n> ")
-                if int(score) >= 1:
-                    self.scores[translator].append({
-                        "score": score,
-                        "id": sentence_id,
-                        "language": language
-                    })
-                    print(f"That translation was from {translator}!")
+            print(f'-'*80)
+            print(f'Statistics for {translator}:')
+            print(f'Mean score: {mean}')
+            print(f'Median score: {all_scores[int(len(all_scores)/2)]}')
+            print(f'# of evaluations: {len(all_scores)}')
+            print(f'Number of unique sentences evaluated: {len(sentences)}')
+            print(f'% of original sentences evaluated: {float(129/len(sentences))}')
+            print(f'-'*80)
 
 
 def main():
     argvs = list(sys.argv)
-    if len(argvs) > 2 and \
-        (argvs[1] =="combine" or argvs[1] == "-c"):
-        eva = Evaluator()
-        eva.combine_scores(argvs[2], argvs[3])
+    if len(argvs) >= 2:
+        if (argvs[1] =="--combine" or argvs[1] == "-c"):
+            combine_scores(argvs[2], argvs[3])
+        elif (argvs[1] == "--statistics" or argvs[1] == '-s'):
+            show_statistics(argvs[2])
     else:
         eva = Evaluator()
         eva.evaluate_translations()
