3b744ebeca26bd3e36be845ce535135bcb83cb24
Sebastian Grunditz <sebgr273@student.liu.se>
2021-03-11 10:36:17 +0100

    fixed skip

--- evaluator.py
+++ evaluator.py
@@ -100,9 +100,10 @@ class Evaluator():
                         "language": language
                     })
                     print(f"That translation was from {translator}!")
-                    self.counter += 1
+                    counter += 1
                 elif int(score) < 0:
                     print("Invalid score!")
+                    counter += 1
         
 
 def combine_scores(original, new):
